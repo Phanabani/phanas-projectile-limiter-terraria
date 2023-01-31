7aceb6c026c7430acecd026bf535bffb5662f868
Phanabani <phanabani@gmail.com>
2023-01-31 01:00:20 -0500

    Set ProjectileLimit's max to the game's hardcoded max.

--- Config/Config.cs
+++ Config/Config.cs
@@ -14,7 +14,7 @@ public class Config : ModConfig
     
     [Label("Projectile Limit")]
     [Tooltip("The maximum number of projectiles allowed in the game")]
-    [Range(0, 10000)]
+    [Range(0, 1000)]
     [DefaultValue(1000)]
     public int ProjectileLimit;
 
